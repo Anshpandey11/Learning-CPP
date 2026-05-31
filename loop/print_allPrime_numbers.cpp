928e3bb4b79321cc75edca6eb8d465101271198b
Anshpandey11 <anshpandey11200630@gmail.com>
2026-05-31 05:45:23 +0530

    Learning

---
+++ loop/print_allPrime_numbers.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 2; i <= n; i++)
+    {
+        int curr = i;
+        bool isPrime = true;
+        for (int j = 2; j * j <= i; j++)
+        {
+            if (curr % j == 0)
+            {
+                isPrime = false;
+            }
+        }
+        if (isPrime)
+        {
+            cout << curr << " ";
+        }
+    }
+    cout << endl;
+}
