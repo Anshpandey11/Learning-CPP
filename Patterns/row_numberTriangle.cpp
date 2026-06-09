cb3f2a74279c5b1351d38da91fe4f0646156bfbb
Anshpandey11 <anshpandey11200630@gmail.com>
2026-06-09 23:29:41 +0530

    Learning

--- Patterns/row_numberTriangle.cpp
+++ Patterns/row_numberTriangle.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
 int main() {
-    int n = 4;
-
+    int n;
+    cin>>n;
     for(int i = 1; i <= n; i++) {
         for(int j = 1; j <= i; j++) {
             cout << i << " ";
